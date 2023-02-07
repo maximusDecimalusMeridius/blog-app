af585e86913768a60610cc857aea4160a8d43b81
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-02-07 12:49:28 -0800

    Connected db and sequelize

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const sequelize = require('./config/connection');
+
 app.get("/", (req, res) => {
     res.send("Hello!");
 })
 
-app.listen(PORT, function() {
-    console.log("App listening on port " + PORT);
-})
+sequelize.sync({force:false}).then(function() {
+    app.listen(PORT, function() {
+        console.log('App listening on PORT ' + PORT);
+    });
+});
