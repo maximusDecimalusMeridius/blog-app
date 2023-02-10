76e9740d3bce4aee59c53b8698aee604b3f95d6f
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-02-09 19:28:21 -0800

    Updated blogRoutes file

--- controllers/blogRoutes.js
+++ controllers/blogRoutes.js
@@ -1,13 +1,43 @@
+//loop in dependencies
 const express = require("express");
 const Blog = require("../models/Blog");
 const router = express.Router();
 
+//GET all records
 router.get("/", (req, res) => {
-    res.send("Router for blogs works!");
+    Blog.findAll().then(data => {
+        res.json(data);
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: "Error getting records!",
+            error: error
+        })
+    })
 })
 
+//GET one record by id
+router.get("/:id", (req, res) => {
+    Blog.findByPk(req.params.id)
+    .then(data => {
+        if(data){
+           return res.json(data);
+        } else {
+            res.status(404).json({
+                message: "No record exists!"
+            })
+        }
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: "Error getting data",
+            error: error
+        })
+    })
+})
+
+//POST a new record
 router.post("/", (req, res) => {
-    console.log(req.session);
     Blog.create({
         title: req.body.title,
         content: req.body.content
@@ -18,6 +48,51 @@ router.post("/", (req, res) => {
     .catch(error => {
         res.status(500).json({
             message: "Error!",
+            error: error
+        })
+    })
+})
+
+//UPDATE a record
+router.put("/:id", (req, res) => {
+    Blog.update({
+        title: req.body.title,
+        content: req.body.content
+    },{
+        where: {
+            id:req.params.id
+        }
+    }).then(data => {
+        if(data[0]){
+            return res.json(data);
+        } else {
+            return res.status(404).json({message: "Record doesn't exist!"});
+        }
+    }).catch(error =>{
+        console.log(error);
+        res.status(500).json({
+            message: "Error updating record!",
+            error: error
+        })
+    })
+})
+
+//DELETE a record
+router.delete("/:id", (req, res) => {
+    Blog.destroy({
+        where:{
+            id:req.params.id
+        }
+    }).then(data=>{
+        if(data){
+            return res.json(data)
+        } else {
+            return res.status(404).json({message: "Record doesn't exist!"})
+        }
+    }).catch(error =>{
+        console.log(error);
+        res.status(500).json({
+            message: "Error deleting record!",
             error: error
         })
     })
