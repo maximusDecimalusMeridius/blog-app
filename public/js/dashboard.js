e582c5ce2649a3f2500629a46c6319db1bbf0ece
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-05 22:25:55 -0800

    Updated id's to classes and added event listeners to all buttons - updated home and dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,10 +3,8 @@ const addBlogForm = document.querySelector(".new-form");
 const submitBlogButton = document.querySelector("#submit-button");
 const blogHeaders = document.querySelectorAll(".blogheader");
 
-const addCommentButton = document.querySelector("#add-comment-button");
-const submitCommentButton = document.querySelector("#submit-comment-button");
-const commentTitle = document.querySelector("#comment-title");
-const commentContent = document.querySelector("#comment-content");
+const addCommentButtons = document.querySelectorAll(".add-comment-button");
+const submitCommentButtons = document.querySelectorAll(".submit-comment-button");
 
 blogHeaders.forEach(blogPost => {
     blogPost.addEventListener("click", function(event) {
@@ -47,40 +45,45 @@ submitBlogButton.addEventListener("click", async (event) => {
     }
 })
 
-addCommentButton.addEventListener("click", (event) =>  {
-    event.target.style.display = "none";
-    event.target.nextElementSibling.style.display = "block";
-})
+for(let i = 0; i < addCommentButtons.length; i++){
+    addCommentButtons[i].addEventListener("click", (event) =>  {
+        event.target.style.display = "none";
+        event.target.nextElementSibling.style.display = "block";
+    })
+}
 
-submitCommentButton.addEventListener("click", async (event) => {
-    event.preventDefault();
-    console.log(event);
-    try{
-       
-        const newCommentObj = {
-            title: commentTitle.value,
-            content: commentContent.value,
-            blog_id: event.target.parentNode.parentNode.parentNode.parentNode.dataset.id
-        }
+for(let i = 0; i < submitCommentButtons.length; i++){
+
+    submitCommentButtons[i].addEventListener("click", async (event) => {
+        event.preventDefault();
+        console.log(event.target.parentNode[0]);
+        try{
         
-        const result = await fetch("/blogs/comments",{
-            method:"POST",
-            body:JSON.stringify(newCommentObj),
-            headers:{
-                "Content-Type":"application/json"
+            const newCommentObj = {
+                title: event.target.parentNode[0].value,
+                content: event.target.parentNode[1].value,
+                blog_id: event.target.parentNode.parentNode.parentNode.parentNode.dataset.id
             }
-        })
+            
+            const result = await fetch("/blogs/comments",{
+                method:"POST",
+                body:JSON.stringify(newCommentObj),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            })
 
-        //reset fields and reload page
-        commentTitle.value = "";
-        commentContent.value = "";
-        if(result.status === 500){
-            alert("Please login before commenting");
-        } else {
-            location.reload();
+            //reset fields and reload page
+            event.target.parentNode[0].value = "";
+            event.target.parentNode[1].value = "";
+            if(result.status === 500){
+                alert("Please login before commenting");
+            } else {
+                location.reload();
+            }
+        } catch(error) {
+            console.error(error);
+            json.status(500)
         }
-    } catch(error) {
-        console.error(error);
-        json.status(500)
-    }
-})
+    })
+}
--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,7 @@
 const blogHeaders = document.querySelectorAll(".blogheader");
 
-const addCommentButton = document.querySelector("#add-comment-button");
-const submitCommentButton = document.querySelector("#submit-comment-button");
-const commentTitle = document.querySelector("#comment-title");
-const commentContent = document.querySelector("#comment-content");
+const addCommentButtons = document.querySelectorAll(".add-comment-button");
+const submitCommentButtons = document.querySelectorAll(".submit-comment-button");
 
 blogHeaders.forEach(blogPost => {
     blogPost.addEventListener("click", function(event) {
@@ -13,39 +11,46 @@ blogHeaders.forEach(blogPost => {
     })
 })
 
-addCommentButton.addEventListener("click", (event) =>  {
-    event.target.style.display = "none";
-    event.target.nextElementSibling.style.display = "block";
-})
+for(let i = 0; i < addCommentButtons.length; i++){
+    addCommentButtons[i].addEventListener("click", (event) =>  {
+        event.target.style.display = "none";
+        event.target.nextElementSibling.style.display = "block";
+    })
+}
 
-submitCommentButton.addEventListener("click", async (event) => {
-    event.preventDefault();
-    try{
-       
-        const newCommentObj = {
-            title: commentTitle.value,
-            content: commentContent.value,
-            blog_id: event.target.parentNode.parentNode.parentNode.parentNode.dataset.id
-        }
+for(let i = 0; i < submitCommentButtons.length; i++){
+
+    submitCommentButtons[i].addEventListener("click", async (event) => {
+        event.preventDefault();
+        console.log(event.target.parentNode[0]);
+        try{
         
-        const result = await fetch("/blogs/comments",{
-            method:"POST",
-            body:JSON.stringify(newCommentObj),
-            headers:{
-                "Content-Type":"application/json"
+            const newCommentObj = {
+                title: event.target.parentNode[0].value,
+                content: event.target.parentNode[1].value,
+                blog_id: event.target.parentNode.parentNode.parentNode.parentNode.dataset.id
             }
-        })
+            
+            const result = await fetch("/blogs/comments",{
+                method:"POST",
+                body:JSON.stringify(newCommentObj),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            })
 
-        //reset fields and reload page
-        commentTitle.value = "";
-        commentContent.value = "";
-        if(result.status === 500){
-            alert("Please login before commenting");
-        } else {
-            location.reload();
+            //reset fields and reload page
+            event.target.parentNode[0].value = "";
+            event.target.parentNode[1].value = "";
+            if(result.status === 500){
+                alert("Please login before commenting");
+            } else {
+                location.reload();
+            }
+        } catch(error) {
+            console.error(error);
+            json.status(500)
         }
-    } catch(error) {
-        console.error(error);
-        json.status(500)
-    }
-})
+    })
+}
+
